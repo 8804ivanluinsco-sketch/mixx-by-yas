e775d392cf19e9ff1b1c89db6e816436877d9ad4
8804ivanluinsco-sketch <8804ivanluinsco@gmail.com>
2026-05-14 23:53:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "/")));
 
 // Add a route to serve your main HTML file
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html")); 
+  res.sendFile(path.join(__dirname, "public", "index.html")); 
 });
 
 
